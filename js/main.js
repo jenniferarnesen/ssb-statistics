a693e8c50f577e10e391d1c61fac2b6d59641700
Jennifer Jones Arnesen <jjarnesen@gmail.com>
2016-02-14 19:10:50 +0100

    start the cleanup job. Begin modularizing and generalizing

--- js/main.js
+++ js/main.js
@@ -11,81 +11,50 @@ require.config({
     }
 });
 
-require(['jQuery', 'jquery-tablesorter'], function ($) {
+require(['jQuery', 'ssb-service', 'jquery-tablesorter'], function ($, ssbService) {
     $(document).ready(function () {
-
-        console.log('ready to go');
 
         $('#statistics').tablesort();
 
-        if (typeof JSONstat !== 'function') {
-            console.log('missing the JSONstat function');
-            throw "missing JSONstat";
-        }
+        ssbService.get()
+            .then(function (ds) {
+                var OSLO = ds.Dimension('Region').id[51],
+                    WOMEN = ds.Dimension('Kjonn').id[2],
+                    years = ds.Dimension('Tid').id,
+                    result = [],
+                    i;
+                
+                //free dimension is Levels (Nivaa)
+                for (i = 0; i < years.length; i += 1) {
+                    var res =
+                        ds.Data({
+                            'Region': OSLO,
+                            'Kjonn': WOMEN,
+                            'Tid': years[i]
+                        })
+                        .map(function (curr) {
+                            return curr.value;
+                        });
 
-        var ssbApi = 'http://data.ssb.no/api/v0/dataset/85432.json?lang=no';
+                    result.push([
+                        ds.Dimension('Tid').Category(i).label, res[0], res[1], res[2], res[3]
+                    ]);
+                };
 
-        JSONstat(ssbApi, function () {
-            console.log('got the data');
-            
-            // var beforeSec = new Date().getTime();
-            // var data1 = this.Dataset(0).toTable( {type : 'arrobj', content : 'id'}, function (d, i) {
+                for (i = 0; i < result.length; i += 1) {
+                    var tds = result[i]
+                        .map(function (curr) {
+                            return '<td>' + curr + '</td>';
+                        })
+                        .reduce(function (prev, curr) {
+                            return prev + curr;
+                        });
 
-            //     if (d.Kjonn === '2' && d.Region === '0301') {
-            //         return {
-            //             percent: d.value,
-            //             year: d.Tid,
-            //             level: d.Nivaa
-            //         } 
-            //     }
-            // });
-            // var afterSec = new Date().getTime();
-            // console.log('time used', afterSec - beforeSec);
-            
-            var ds = this.Dataset(0);
-                OSLO = ds.Dimension('Region').id[51],
-                levels = ds.Dimension('Nivaa').id,
-                WOMEN = ds.Dimension('Kjonn').id[2],
-                years = ds.Dimension('Tid').id,
-                result = [];
-            
-            //free dimension is Levels (Nivaa)
-            var transform = function (curr, i) {
-                return curr.value;
-            };
-            var beforeSec1 = new Date().getTime();
-            for (var i = 0; i < years.length; i += 1) {
-                var res = ds.Data({
-                    'Region': OSLO,
-                    'Kjonn': WOMEN,
-                    'Tid': years[i]
-                }).map(transform);
-
-                result.push([
-                    ds.Dimension('Tid').Category(i).label, res[0], res[1], res[2], res[3]
-                ]);
-            };
-            var afterSec1 = new Date().getTime();
-            console.log('time used', afterSec1 - beforeSec1);
-
-            for (var j = 0; j < result.length; j += 1) {
-                var item = result[j];
-                var trMarkup =
-                    '<tr> \
-                        <td>' + item[0] + '</td> \
-                        <td>' + item[1] + '</td> \
-                        <td>' + item[2] + '</td> \
-                        <td>' + item[3] + '</td> \
-                        <td>' + item[4] + '</td> \
-                    </tr>';
-                $('#statistics tbody').append(trMarkup); 
-            }
-
-            //get education level for women in Oslo, by year
-            // year | grunn | videre | uni1 | uni2
-            // -----------------------------------
-            // 1980 |   33  |   22   | 20   |  25
-            // 1981 |   35  |   25   | 25   |  15 
-        });
+                    $('#statistics tbody').append('<tr>' + tds + '</tr>'); 
+                }
+            })
+            .catch(function (e) {
+                console.log('could not fetch data at this time', e);
+            });
     });
 });
